b07bc2e627cc6d874bff567bb93c55cd071652f4
Gabriel Betancourt <gbcarrasco99@gmail.com>
2018-06-25 02:11:59 -0400

    Haciendo cambios en el servidor principal

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 
 const express = require('express');
 const morgan = require ('morgan');
+const bodyParser = require('body-parser');
+
+const server = express();
+server.use(bodyParser.json());
+
+process.env.NODE_ENV = 'development' //'production'
+let io = require("socket.io");
+let socketServer = io.listen(9999, {
+    "log level": 1
+});
+require('./routes')(socketServer);
+
+
